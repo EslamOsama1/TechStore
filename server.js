8435ea0a403083544a10941db36d951095337f87
EslamOsama1 <asamhaslam062@gmail.com>
2026-08-11 19:06:21 +0300

    .\.git\edit Port

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ mongoose.connect(DB).then(() => console.log("DB connection successfully"))
 const PORT = process.env.PORT || 3000
 
 const server = app.listen(PORT, () => {
-    console.log('server running on port : 3000')
+    console.log(`server running on port : ${PORT}`)
 })
